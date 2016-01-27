846eb0d1df43b39ac41c39b787dd3251b8323870
Kristofer Rye <kristofer.rye@gmail.com>
2016-01-26 20:39:39 -0600

    Clean up things
    I'm gonna restructure it again.  Woo, iteration.

--- src/org/usfirst/frc/team2503/r2016/Main.java
+++ src/org/usfirst/frc/team2503/r2016/Main.java
@@ -1,25 +1,11 @@
 package org.usfirst.frc.team2503.r2016;
 
-import org.usfirst.frc.team2503.r2016.input.NullInput;
-import org.usfirst.frc.team2503.r2016.input.NullMonitor;
-
 /**
  * You can use this class for testing non-WPIlib code on your machine.
  */
 public class Main {
 
 	public static void main(String[] args) {
-		NullInput nullInput = new NullInput();
-		NullMonitor nullMonitor = new NullMonitor(nullInput);
-		
-		Thread thread = new Thread(nullMonitor);
-		thread.start();
-		
-		try {
-			thread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 	}
 	
 }
--- src/org/usfirst/frc/team2503/r2016/input/DataSource.java
+++
@@ -1,7 +0,0 @@
-package org.usfirst.frc.team2503.r2016.input;
-
-public abstract class DataSource {
-
-	public abstract Data getData();
-
-}

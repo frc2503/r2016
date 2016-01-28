43fba2134d8d9c6a84aa22192807fce78bfda91d
Kristofer Rye <kristofer.rye@gmail.com>
2016-01-27 23:35:11 -0600

    Main: Add example code

--- src/org/usfirst/frc/team2503/r2016/Main.java
+++ src/org/usfirst/frc/team2503/r2016/Main.java
@@ -1,11 +1,28 @@
 package org.usfirst.frc.team2503.r2016;
 
+import org.usfirst.frc.team2503.r2016.web.WebConfiguration;
+
 /**
  * You can use this class for testing non-WPIlib code on your machine.
  */
 public class Main {
 
 	public static void main(String[] args) {
+
+		WebConfiguration wc = new WebConfiguration();
+
+		Thread t = new Thread(wc.getServerInstance());
+
+		System.out.println("[main] Starting Thread...");
+		t.start();
+		System.out.println("[main] Thread started!");
+
+		try {
+			t.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }

968cc9f0bda3c6546a20674f95dbbab74719402a
Kristofer Rye <kristofer.rye@gmail.com>
2016-01-30 12:12:58 -0600

    Robot: Add three servers

--- src/org/usfirst/frc/team2503/r2016/Robot.java
+++ src/org/usfirst/frc/team2503/r2016/Robot.java
@@ -1,10 +1,27 @@
 package org.usfirst.frc.team2503.r2016;
 
+import org.usfirst.frc.team2503.r2016.data.DataServer;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
 
+	public DataServer configurationServer;
+	public DataServer imageServer;
+	public DataServer dataServer;
+
 	public Robot() {
+		configurationServer = new DataServer(5800);
+		imageServer = new DataServer(5801);
+		dataServer = new DataServer(5802);
+
+		Thread configurationServerThread = new Thread(configurationServer.getServerInstance());
+		Thread imageServerThread = new Thread(imageServer.getServerInstance());
+		Thread dataServerThread = new Thread(dataServer.getServerInstance());
+
+		configurationServerThread.start();
+		imageServerThread.start();
+		dataServerThread.start();
 	}
 
 	public void robotInit() {

7db8b042b62aa37b0ecea67f27b5e40ee781ed4b
Kristofer Rye <kristofer.rye@gmail.com>
2016-01-30 10:52:30 -0600

    Robot: Delete trailing whitespace

--- src/org/usfirst/frc/team2503/r2016/Robot.java
+++ src/org/usfirst/frc/team2503/r2016/Robot.java
@@ -3,35 +3,35 @@ package org.usfirst.frc.team2503.r2016;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
-	
+
 	public Robot() {
 	}
-	
+
 	public void robotInit() {
 	}
-	
+
 	public void disabledInit() {
 	}
-	
+
 	public void disabledPeriodic() {
 	}
-	
+
 	public void autonomousInit() {
 	}
-	
+
 	public void autonomousPeriodic() {
 	}
 
 	public void teleopInit() {
 	}
-	
+
 	public void teleopPeriodic() {
 	}
-	
+
 	public void testInit() {
 	}
-	
+
 	public void testPeriodic() {
 	}
-	
+
 }

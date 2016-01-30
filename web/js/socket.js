f1062cf3977db91ec257b63fd11e825f40df52aa
Kristofer Rye <kristofer.rye@gmail.com>
2016-01-30 11:18:04 -0600

    Socket: Fix connection pipeline to be persistent in attempting to connect

--- web/js/socket.js
+++ web/js/socket.js
@@ -11,19 +11,42 @@ switch(window.DEVELOPMENT_MODE) {
 		break;
 }
 
-window.SOCKET = new Socket("ws://" + window.SOCKET_URI + ":5800/");
+function setupSocket(key, uri, messageCallback) {
+	window[key] = new Socket(uri, (function() {
+		setupSocket(key, uri, messageCallback);
+	}), messageCallback);
+}
 
-function Socket(url) {
+function Socket(url, setupCallback, messageCallback) {
 	this.url = url;
 	this.socket = new WebSocket(url);
+	this.setupCallback = setupCallback;
+	this.messageCallback = messageCallback;
 
 	/* TODO: Implement */
-	this.socket.onopen = null;
-	this.socket.onmessage = null;
-	this.socket.onerror = null;
-	this.socket.onclose = null;
+	this.socket.onopen = function() {
+		console.debug(arguments);
+	};
+
+	this.socket.onmessage = function(messageEvent) {
+		this.messageCallback(messageEvent);
+	};
+
+	this.socket.onerror = function() {
+		console.debug(arguments);
+	};
+
+	this.socket.onclose = function() {
+		setTimeout(function() {
+			this.setupCallback();
+		}.bind(this), 500);
+	}.bind(this);
 
 	this.send = function(data) {
 		this.socket.send(data);
 	}
 }
+
+setupSocket('configuration_socket', 'ws://' + window.SOCKET_URI + ':5800/', function(messageEvent) {
+	console.debug(messageEvent.data);
+});

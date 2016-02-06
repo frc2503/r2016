d7c19bf201b9aa6f28d293c7ae795c578fa92f87
Kristofer Rye <kristofer.rye@gmail.com>
2016-02-06 10:18:15 -0600

    Delete the unused duplicate Data class
    I prefer the JSONObject specification to a HashMap because serialization is nice.

--- src/org/usfirst/frc/team2503/r2016/input/Data.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team2503.r2016.input;
-
-import java.util.HashMap;
-
-public class Data extends HashMap<String, Object> {
-
-	private static final long serialVersionUID = 1L;
-
-}

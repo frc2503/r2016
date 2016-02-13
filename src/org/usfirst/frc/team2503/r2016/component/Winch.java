5efd9b526921f3a4461a72ca194a9792b40495b1
Kristofer Rye <kristofer.rye@gmail.com>
2016-02-13 10:17:57 -0600

    Stick Winch into the hierarchy of Components

--- src/org/usfirst/frc/team2503/r2016/component/Winch.java
+++ src/org/usfirst/frc/team2503/r2016/component/Winch.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team2503.r2016.component;
 
 import edu.wpi.first.wpilibj.SpeedController;
 
-public class Winch {
+public class Winch implements SpeedController, Component {
 
 	public SpeedController controller;
 

81816d3c2edcd786df64812c9721ec18763047f8
Kristofer Rye <kristofer.rye@gmail.com>
2016-02-25 15:55:37 -0600

    Clean up trailing whitespace in a bunch of files

--- src/org/usfirst/frc/team2503/r2016/component/Hooker.java
+++ src/org/usfirst/frc/team2503/r2016/component/Hooker.java
@@ -5,31 +5,31 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class Hooker extends SpeedControllerComponent {
-	
+
 	public DigitalInput limitSwitch;
 	public Encoder encoder;
-	
+
 	public Hooker(SpeedController controller) {
 		super(controller);
 	}
-	
+
 	public Hooker(SpeedController controller, DigitalInput limitSwitch) {
 		super(controller);
-		
+
 		this.limitSwitch = limitSwitch;
 	}
-	
+
 	public Hooker(SpeedController controller, Encoder encoder) {
 		super(controller);
-		
+
 		this.encoder = encoder;
 	}
-	
+
 	public Hooker(SpeedController controller, Encoder encoder, DigitalInput limitSwitch) {
 		super(controller);
-		
+
 		this.encoder = encoder;
 		this.limitSwitch = limitSwitch;
 	}
-	
+
 }
--- src/org/usfirst/frc/team2503/r2016/component/Shooter.java
+++ src/org/usfirst/frc/team2503/r2016/component/Shooter.java
@@ -3,9 +3,9 @@ package org.usfirst.frc.team2503.r2016.component;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class Shooter extends SpeedControllerComponent {
-	
+
 	public Shooter(SpeedController controller) {
 		super(controller);
 	}
-	
+
 }

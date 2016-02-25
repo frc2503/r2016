5cf8cce55464cef2813c57b811c86cfcc441e45f
Kristofer Rye <kristofer.rye@gmail.com>
2016-02-25 15:53:12 -0600

    Clean up whitespace and add radian-degree conversion math

--- src/org/usfirst/frc/team2503/lib/util/WarriorMath.java
+++ src/org/usfirst/frc/team2503/lib/util/WarriorMath.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team2503.lib.util;
 
 public class WarriorMath {
-	
+
 	static final double sq2p1 = 2.414213562373095048802e0;
 	static final double sq2m1 = .414213562373095048802e0;
 	static final double p4 = .161536412982230228262e2;
@@ -19,30 +19,30 @@ public class WarriorMath {
 
 	private static double mxatan(double arg) {
 		double argsq, value;
-		
+
 		argsq = arg * arg;
 		value = ((((p4 * argsq + p3) * argsq + p2) * argsq + p1) * argsq + p0);
 		value = value / (((((argsq + q4) * argsq + q3) * argsq + q2) * argsq + q1) * argsq + q0);
 		return value * arg;
 	}
-	
+
 	private static double msatan(double arg) {
 		if (arg < sq2m1) {
 			return mxatan(arg);
 		}
-		
+
 		if (arg > sq2p1) {
 			return PIO2 - mxatan(1 / arg);
 		}
-		
+
 		return PIO2 / 2 + mxatan((arg - 1) / (arg + 1));
 	}
-	
+
 	public static double atan(double arg) {
 		if (arg > 0) {
 			return msatan(arg);
 		}
-		
+
 		return -msatan(-arg);
 	}
 
@@ -51,34 +51,34 @@ public class WarriorMath {
 			if (arg1 >= 0) {
 				return PIO2;
 			}
-			
+
 			return -PIO2;
 		}
-		
+
 		arg1 = atan(arg1 / arg2);
 
 		if (arg2 < 0) {
 			if (arg1 <= 0) {
 				return arg1 + Math.PI;
 			}
-			
+
 			return arg1 - Math.PI;
 		}
-		
+
 		return arg1;
 	}
 
 	public static double asin(double arg) {
 		double temp;
 		int sign;
-		
+
 		sign = 0;
-		
+
 		if (arg < 0) {
 			arg = -arg;
 			sign++;
 		}
-		
+
 		if (arg > 1) {
 			return nan;
 		}
@@ -90,80 +90,87 @@ public class WarriorMath {
 		} else {
 			temp = atan(arg / temp);
 		}
-		
+
 		if (sign > 0) {
 			temp = -temp;
 		}
-		
+
 		return temp;
 	}
-	
+
 	public static double acos(double arg) {
 		if (arg > 1 || arg < -1) {
 			return nan;
 		}
-		
+
 		return PIO2 - asin(arg);
 	}
-	
+
 	public static double getDifferenceInAngleRadians(double from, double to) {
 		return boundAngleNegPiToPiRadians(to - from);
 	}
-	
+
 
 	public static double getDifferenceInAngleDegrees(double from, double to) {
 		return boundAngleNeg180to180Degrees(to - from);
 	}
-	
+
 	public static double boundAngle0to360Degrees(double angle) {
 		while (angle >= 360.0) {
 			angle -= 360.0;
 		}
-		
+
 		while (angle < 0.0) {
 			angle += 360.0;
 		}
 
 		return angle;
 	}
-	
+
 	public static double boundAngleNeg180to180Degrees(double angle) {
 		while (angle >= 180.0) {
 			angle -= 360.0;
 		}
-		
+
 		while (angle < -180.0) {
 			angle += 360.0;
 		}
-		
+
 		return angle;
 	}
-	
+
 	public static double boundAngle0to2PiRadians(double angle) {
 		while (angle >= 2.0 * Math.PI) {
 			angle -= 2.0 * Math.PI;
 		}
-		
+
 		while (angle < 0.0) {
 			angle += 2.0 * Math.PI;
 		}
-		
+
 		return angle;
 	}
-	
+
 	public static double boundAngleNegPiToPiRadians(double angle) {
 		while (angle >= Math.PI) {
 			angle -= 2.0 * Math.PI;
 		}
-		
+
 		while (angle < -Math.PI) {
 			angle += 2.0 * Math.PI;
 		}
-		
+
 		return angle;
 	}
-		
+
+	public static double radiansToDegrees(double radians) {
+		return (180.0d / Math.PI) * radians;
+	}
+
+	public static double degreesToRadians(double degrees) {
+		return (Math.PI / 180.0d) * degrees;
+	}
+
 	public WarriorMath() {
 	}
 }
-

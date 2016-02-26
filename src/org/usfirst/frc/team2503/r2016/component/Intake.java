9b3ea8f283a618e46340652cf4f2f3dc79884e30
Kristofer Rye <kristofer.rye@gmail.com>
2016-02-25 23:48:00 -0600

    Intake: Remove superfluous whitespace

--- src/org/usfirst/frc/team2503/r2016/component/Intake.java
+++ src/org/usfirst/frc/team2503/r2016/component/Intake.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class Intake extends SpeedControllerComponent {
-	
+
 	public enum IntakeMode {
 		UNKNOWN,
 		STOPPED,
@@ -16,43 +16,43 @@ public class Intake extends SpeedControllerComponent {
 	public DigitalInput limitSwitch;
 
 	private IntakeMode mode = IntakeMode.UNKNOWN;
-	
+
 	public void setMode(IntakeMode mode) { this.mode = mode; }
 	public IntakeMode getMode() { return this.mode; }
-	
+
 	public void tick() {
 		switch(this.mode) {
 		case STOPPED:
 			this.controller.set(0.0);
 			break;
-			
+
 		case INBOUND:
 			if(this.limitSwitch.get()) {
 				this.controller.set(0.0);
 			} else {
 				this.controller.set(1.0);
 			}
-			
+
 			break;
-			
+
 		case OUTBOUND:
 			this.controller.set(-1.0);
 			break;
-			
+
 		case FIRE:
 			this.controller.set(1.0);
 			break;
-		
+
 		case UNKNOWN:
 		default:
 			break;
 		}
 	}
-	
+
 	public Intake(SpeedController controller, DigitalInput limitSwitch) {
 		super(controller);
-		
+
 		this.limitSwitch = limitSwitch;
 	}
-	
+
 }

438d08caf5b3df68c8bdeb22e285596ab0f73ed6
Kristofer Rye <kristofer.rye@gmail.com>
2016-02-28 16:05:43 -0600

    MadCatzV1Joystick: Remove some stupid trailing whitespace

--- src/org/usfirst/frc/team2503/r2016/input/joystick/MadCatzV1Joystick.java
+++ src/org/usfirst/frc/team2503/r2016/input/joystick/MadCatzV1Joystick.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team2503.r2016.input.joystick;
 
 public class MadCatzV1Joystick extends Joystick {
-	
+
 	public ControllerButton trigger = new ControllerButton(1);
 	public ControllerButton button2 = new ControllerButton(2);
 	public ControllerButton button3 = new ControllerButton(3);
@@ -9,9 +9,9 @@ public class MadCatzV1Joystick extends Joystick {
 	public ControllerButton button5 = new ControllerButton(5);
 	public ControllerButton button6 = new ControllerButton(6);
 	public ControllerButton gripButton = new ControllerButton(7);
-	
+
 	public ControllerPOV pov = new ControllerPOV();
-	
+
 	public ControllerAxis x = new ControllerAxis(0, ControllerAxisType.NEGATIVE_TO_POSITIVE);
 	public ControllerAxis y = new ControllerAxis(1, ControllerAxisType.NEGATIVE_TO_POSITIVE);
 	public ControllerAxis throttle = new ControllerAxis(2, ControllerAxisType.ZERO_TO_POSITIVE);
@@ -26,7 +26,7 @@ public class MadCatzV1Joystick extends Joystick {
 				(button6.get() ? "6" : " ") +
 				(gripButton.get() ? "G" : " ");
 	}
-	
+
 	public MadCatzV1Joystick(int port) {
 		super(port);
 	}

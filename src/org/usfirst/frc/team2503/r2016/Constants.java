07481326f9f63b32535ba08ef468f04475599c46
Kristofer Rye <kristofer.rye@gmail.com>
2016-02-28 15:52:33 -0600

    Remove excess imports from Constants

--- src/org/usfirst/frc/team2503/r2016/Constants.java
+++ src/org/usfirst/frc/team2503/r2016/Constants.java
@@ -1,16 +1,5 @@
 package org.usfirst.frc.team2503.r2016;
 
-import org.usfirst.frc.team2503.r2016.component.CameraMount.CameraLights;
-
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Relay;
-import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.Talon;
-
 public class Constants {
 	
 	public static final String defaultHostname = "0.0.0.0";

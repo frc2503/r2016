594829ae85b2d690ed0a8a3f0b09a1e64b11f623
Kristofer Rye <kristofer.rye@gmail.com>
2016-02-28 14:14:09 -0600

    Remove unused Main.java changes, create a Logger test

--- src/org/usfirst/frc/team2503/r2016/Main.java
+++ src/org/usfirst/frc/team2503/r2016/Main.java
@@ -1,9 +1,7 @@
 package org.usfirst.frc.team2503.r2016;
 
-import java.net.InetSocketAddress;
-
-import org.usfirst.frc.team2503.r2016.server.DataServer;
-import org.usfirst.frc.team2503.r2016.server.MessageServer;
+import org.usfirst.frc.team2503.r2016.debug.Logger;
+import org.usfirst.frc.team2503.r2016.debug.Logger.LoggerPrintStream;
 
 /**
  * You can use this class for testing non-WPIlib code on your machine.
@@ -11,40 +9,12 @@ import org.usfirst.frc.team2503.r2016.server.MessageServer;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.setProperty("java.awt.headless", "true");
 		
-//		Logger.addPrintStream(new LoggerPrintStream(System.out));
-//		
-//		Camera camera = new Camera(0);
-//		try {
-//			camera.start();
-//			camera.grabFrame();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		
-//		Logger.println(camera.getLatestFrame());
-//		
-//		Logger.println("foo");
+		Logger.addPrintStream(new LoggerPrintStream(System.out));
+		Logger.addPrintStream(new LoggerPrintStream(System.err));
 		
-		Thread dataServerThread = null;
-		Thread messageServerThread = null;
+		Logger.println("foo");
 		
-		DataServer ds = new DataServer(new InetSocketAddress(5800));
-		MessageServer ms = new MessageServer(new InetSocketAddress(5801));
-		
-		dataServerThread = new Thread(ds);
-		messageServerThread = new Thread(ms);
-
-		dataServerThread.start();
-		messageServerThread.start();
-		
-		try {
-			dataServerThread.join();
-			messageServerThread.join();
-		} catch(InterruptedException ex) {
-			ex.printStackTrace(System.err);
-		}
 	}
 	
 }

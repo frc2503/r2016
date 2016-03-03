f03bba0225caa9698375d840d86867eb66591eea
Kristofer Rye <kristofer.rye@gmail.com>
2016-03-02 21:48:36 -0600

    Clean up a couple of whitespace errors, add a comment

--- src/org/usfirst/frc/team2503/r2016/input/MadCatzV1JoystickMadCatzV1JoystickLogitechF310GamepadControlLayout.java
+++ src/org/usfirst/frc/team2503/r2016/input/MadCatzV1JoystickMadCatzV1JoystickLogitechF310GamepadControlLayout.java
@@ -4,13 +4,15 @@ import org.usfirst.frc.team2503.r2016.input.gamepad.Gamepad;
 import org.usfirst.frc.team2503.r2016.input.joystick.Joystick;
 
 public class MadCatzV1JoystickMadCatzV1JoystickLogitechF310GamepadControlLayout extends JoystickJoystickGamepadControlLayout {
-	
+
 	public Data getData() {
 		Data data = super.getData();
-		
+
+		// Any modifications to be made should be done here.
+
 		return data;
 	}
-	
+
 	public MadCatzV1JoystickMadCatzV1JoystickLogitechF310GamepadControlLayout(Joystick left, Joystick right, Gamepad gamepad) {
 		super(left, right, gamepad);
 	}

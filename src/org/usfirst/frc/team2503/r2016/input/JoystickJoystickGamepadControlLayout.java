bd78f314416535d24975247a6614178e98353a56
Kristofer Rye <kristofer.rye@gmail.com>
2016-03-02 21:47:46 -0600

    JoystickJoystickGamepadControlLayout: Implement getData()

--- src/org/usfirst/frc/team2503/r2016/input/JoystickJoystickGamepadControlLayout.java
+++ src/org/usfirst/frc/team2503/r2016/input/JoystickJoystickGamepadControlLayout.java
@@ -4,11 +4,11 @@ import org.usfirst.frc.team2503.r2016.input.gamepad.Gamepad;
 import org.usfirst.frc.team2503.r2016.input.joystick.Joystick;
 
 public abstract class JoystickJoystickGamepadControlLayout implements ControlLayout {
-	
+
 	private Joystick left;
 	private Joystick right;
 	private Gamepad gamepad;
-	
+
 	public JoystickJoystickGamepadControlLayout(Joystick left, Joystick right, Gamepad gamepad) {
 		this.left = left;
 		this.right = right;
@@ -19,14 +19,20 @@ public abstract class JoystickJoystickGamepadControlLayout implements ControlLay
 		Data leftData = this.left.getData();
 		Data rightData = this.right.getData();
 		Data gamepadData = this.gamepad.getData();
-		
+
 		Data data = new Data();
-		
-		data.put("left", leftData);
-		data.put("right", rightData);
-		data.put("gamepad", gamepadData);
-		
+
+		{
+			Data joystickData = new Data();
+
+			joystickData.put("left", leftData);
+			joystickData.put("right", rightData);
+			joystickData.put("gamepad", gamepadData);
+
+			data.put("joysticks", joystickData);
+		}
+
 		return data;
 	}
-	
+
 }

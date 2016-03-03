3c9d16c9386ada45fb79d7c39935dfe13402bbaa
Kristofer Rye <kristofer.rye@gmail.com>
2016-03-02 21:49:34 -0600

    Hooker: Add a little bit of functionality code

--- src/org/usfirst/frc/team2503/r2016/component/Hooker.java
+++ src/org/usfirst/frc/team2503/r2016/component/Hooker.java
@@ -8,10 +8,20 @@ import edu.wpi.first.wpilibj.SpeedController;
 
 public class Hooker extends SpeedControllerComponent {
 
+	public enum HookerMode {
+		RESET,
+		HORIZONTAL,
+		READY,
+		ATTACH
+	}
+
 	public DigitalInput limitSwitch;
 	public Encoder encoder;
 
 	public void tick(Data data) {
+		// If the limit switch is pressed, reset the encoder to 0
+		if(this.limitSwitch.get())
+			this.encoder.reset();
 	}
 
 	public Hooker(SpeedController controller, Encoder encoder, DigitalInput limitSwitch) {

c7696dec04cc94de657a888a630fa96452e24442
Kristofer Rye <kristofer.rye@gmail.com>
2016-03-02 21:47:11 -0600

    MadCatzV1Joystick: Add DataReporter class for data collection

--- src/org/usfirst/frc/team2503/r2016/input/joystick/MadCatzV1Joystick.java
+++ src/org/usfirst/frc/team2503/r2016/input/joystick/MadCatzV1Joystick.java
@@ -19,12 +19,48 @@ public class MadCatzV1Joystick extends Joystick {
 	public ControllerAxis throttle = new ControllerAxis(2, ControllerAxisType.ZERO_TO_POSITIVE);
 	public ControllerAxis rotation = new ControllerAxis(3, ControllerAxisType.NEGATIVE_TO_POSITIVE);
 
+	public class DataReporter {
+		private Data data = new Data();
+		private Data buttons = new Data();
+		private Data axes = new Data();
+
+		public void update() {
+			this.buttons.put("trigger", trigger.get());
+			this.buttons.put("button2", button2.get());
+			this.buttons.put("button3", button3.get());
+			this.buttons.put("button4", button4.get());
+			this.buttons.put("button5", button5.get());
+			this.buttons.put("button6", button6.get());
+			this.buttons.put("gripButton", gripButton.get());
+
+			this.axes.put("x", x.get());
+			this.axes.put("y", y.get());
+			this.axes.put("throttle", throttle.get());
+			this.axes.put("rotation", rotation.get());
+		}
+
+		public Data report() {
+			return this.data;
+		}
+
+		public void compile() {
+			this.data.put("buttons", this.buttons);
+			this.data.put("axes", this.axes);
+		}
+	}
+
+	private DataReporter DataReporter;
+
 	public Data getData() {
-		return null;
+		DataReporter.update();
+		DataReporter.compile();
+		return DataReporter.report();
 	}
 
 	public MadCatzV1Joystick(int port) {
 		super(port);
+
+		this.DataReporter = new DataReporter();
 	}
 
 }

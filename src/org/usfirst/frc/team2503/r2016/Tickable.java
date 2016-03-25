baa0e77dc25b3a14e1a320176ba748cd846ece8f
Kristofer Rye <kristofer.rye@gmail.com>
2016-03-25 12:07:36 -0500

    Move Tickable into new *.r2016.control package

--- src/org/usfirst/frc/team2503/r2016/Tickable.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team2503.r2016;
-
-import org.usfirst.frc.team2503.r2016.data.Data;
-
-public interface Tickable {
-
-	public abstract void tick(Data data);
-
-}
---
+++ src/org/usfirst/frc/team2503/r2016/control/Tickable.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team2503.r2016.control;
+
+public interface Tickable {
+
+	public abstract void tick();
+
+}

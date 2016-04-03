65adb993e9f04a3fb5a202c8e693ba710003cc04
Kristofer Rye <kristofer.rye@gmail.com>
2016-04-03 14:38:56 -0500

    WinchSubsystem: Add some comments and @Override for tick()

--- src/org/usfirst/frc/team2503/r2016/subsystem/WinchSubsystem.java
+++ src/org/usfirst/frc/team2503/r2016/subsystem/WinchSubsystem.java
@@ -18,22 +18,42 @@ public class WinchSubsystem extends ModalSpeedControllerSubsystem {
 		LOWERING
 	}
 	
+	@Override
 	public void tick() {
 		WinchSubsystemMode mode = (WinchSubsystemMode) this.getMode();
 		
 		switch(mode) {
+		/**
+		 * The robot is heavy enough that, if the ratchet is
+		 * not engaged, it will slowly drift downwards.
+		 * 
+		 * Thus, we can apply a small amount of power (5%) to
+		 * apply a bit of vertical lifting power.
+		 */
 		case BASIC_SUSPENSION_WINCHING:
 			this._controller.set(0.0500d);
 			break;
 			
+		/**
+		 * This mode is for when we are lifting slowly, or trying
+		 * to get the hook off before winching at full power.
+		 */
 		case SLOW_WINCHING:
 			this._controller.set(0.2000d);
 			break;
 			
+		/**
+		 * This mode is for when we are lifting quickly, or trying
+		 * to scale ASAP.
+		 */
 		case WINCHING:
 			this._controller.set(1.0000d);
 			break;
-			
+
+		/**
+		 * This mode is for when we are lowering the robot (with the
+		 * ratchet not set)
+		 */
 		case LOWERING:
 			this._controller.set(-0.3000d);
 			break;

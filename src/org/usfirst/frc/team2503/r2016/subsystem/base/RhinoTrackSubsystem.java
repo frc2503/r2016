600090e9572550f4a3cbbb99711dfcee21e1d5b4
Kristofer Rye <kristofer.rye@gmail.com>
2016-04-04 09:25:18 -0500

    Move RhinoTrackSubsystem

--- src/org/usfirst/frc/team2503/r2016/subsystem/base/RhinoTrackSubsystem.java
+++ src/org/usfirst/frc/team2503/r2016/subsystem/base/RhinoTrackSubsystem.java
@@ -1,7 +1,4 @@
-package org.usfirst.frc.team2503.r2016.subsystem;
-
-import org.usfirst.frc.team2503.r2016.subsystem.base.DataSpeedControllerSubsystem;
-import org.usfirst.frc.team2503.r2016.subsystem.base.DataSubsystem;
+package org.usfirst.frc.team2503.r2016.subsystem.base;
 
 import edu.wpi.first.wpilibj.SpeedController;
 
@@ -10,20 +7,18 @@ public class RhinoTrackSubsystem extends DataSpeedControllerSubsystem {
 	public enum RhinoTrackSubsystemDataKey implements DataSubsystem.SubsystemDataKey {
 		POWER
 	}
-	
-	
-	
+
 	@Override
 	public void tick() {
 		double power = (double) this.getDataKey(RhinoTrackSubsystemDataKey.POWER);
 		
-		this._controller.set(power);
+		this.set(power);
 	}
 	
 	public void setPower(double power) {
 		this.setDataKey(RhinoTrackSubsystemDataKey.POWER, power);
 	}
-	
+
 	
 	
 	public void setDataKey(RhinoTrackSubsystemDataKey key, Object value) {
@@ -35,11 +30,11 @@ public class RhinoTrackSubsystem extends DataSpeedControllerSubsystem {
 	}
 	
 	
-
+	
 	public RhinoTrackSubsystem(SpeedController _controller) {
 		super(_controller);
 	}
-	
+
 	public RhinoTrackSubsystem(SpeedControllerSubsystemType type, final int channel) {
 		super(type, channel);
 	}

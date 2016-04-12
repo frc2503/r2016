2848f85c22c4c2de36455e846083a80451f8b827
Kristofer Rye <kristofer.rye@gmail.com>
2016-04-12 18:09:57 -0500

    WarriorMath: Start adding a few comments, cleaning

--- src/org/usfirst/frc/team2503/lib/util/WarriorMath.java
+++ src/org/usfirst/frc/team2503/lib/util/WarriorMath.java
@@ -2,29 +2,34 @@ package org.usfirst.frc.team2503.lib.util;
 
 public class WarriorMath {
 
-	static final double squareRootOf2Plus1 = 2.414213562373095048802e0d;
-	static final double squareRootOf2Minus1 = .414213562373095048802e0d;
-	static final double p4 = .161536412982230228262e2d;
-	static final double p3 = .26842548195503973794141e3d;
-	static final double p2 = .11530293515404850115428136e4d;
-	static final double p1 = .178040631643319697105464587e4d;
-	static final double p0 = .89678597403663861959987488e3d;
-	static final double q4 = .5895697050844462222791e2d;
-	static final double q3 = .536265374031215315104235e3d;
-	static final double q2 = .16667838148816337184521798e4d;
-	static final double q1 = .207933497444540981287275926e4d;
-	static final double q0 = .89678597403663861962481162e3d;
-	static final double PI_OVER_2 = 1.5707963267948966135e0d;
-	static final double NaN	 = (0.0 / 0.0);
+	// TODO: Better variable names
+	public static final double squareRootOf2Plus1 = 2.414213562373095048802e0d;
+	public static final double squareRootOf2Minus1 = .414213562373095048802e0d;
+	public static final double p4 = .161536412982230228262e2d;
+	public static final double p3 = .26842548195503973794141e3d;
+	public static final double p2 = .11530293515404850115428136e4d;
+	public static final double p1 = .178040631643319697105464587e4d;
+	public static final double p0 = .89678597403663861959987488e3d;
+	public static final double q4 = .5895697050844462222791e2d;
+	public static final double q3 = .536265374031215315104235e3d;
+	public static final double q2 = .16667838148816337184521798e4d;
+	public static final double q1 = .207933497444540981287275926e4d;
+	public static final double q0 = .89678597403663861962481162e3d;
+	public static final double PI_OVER_2 = 1.5707963267948966135e0d;
+	public static final double NaN	 = (0.0 / 0.0);
 
-	private static double mxatan(double value) {
+	// TODO: Document
+	// TODO: Improve, clean
+	public static double mxatan(double value) {
 		double valueSquared;
 
 		valueSquared = value * value;
 		return ((((p4 * valueSquared + p3) * valueSquared + p2) * valueSquared + p1) * valueSquared + p0) / (((((valueSquared + q4) * valueSquared + q3) * valueSquared + q2) * valueSquared + q1) * valueSquared + q0) * value;
 	}
 
-	private static double msatan(double arg) {
+	// TODO: Document
+	// TODO: Improve, clean
+	public static double msatan(double arg) {
 		if (arg < squareRootOf2Minus1) {
 			return mxatan(arg);
 		}
@@ -36,6 +41,8 @@ public class WarriorMath {
 		return PI_OVER_2 / 2 + mxatan((arg - 1) / (arg + 1));
 	}
 
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double atan(double arg) {
 		if (arg > 0) {
 			return msatan(arg);
@@ -44,6 +51,8 @@ public class WarriorMath {
 		return -msatan(-arg);
 	}
 
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double atan2(double arg1, double arg2) {
 		if (arg1 + arg2 == arg1) {
 			if (arg1 >= 0) {
@@ -66,6 +75,8 @@ public class WarriorMath {
 		return arg1;
 	}
 
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double asin(double arg) {
 		double temp;
 		int sign;
@@ -96,6 +107,8 @@ public class WarriorMath {
 		return temp;
 	}
 
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double acos(double arg) {
 		if (arg > 1 || arg < -1) {
 			return NaN;
@@ -108,11 +121,14 @@ public class WarriorMath {
 		return boundAngleNegativePiToPiRadians(to - from);
 	}
 
-
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double getDifferenceInAngleDegrees(double from, double to) {
 		return boundAngleNegative180to180Degrees(to - from);
 	}
 
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double boundAngle0to360Degrees(double angle) {
 		while (angle >= 360.0) {
 			angle -= 360.0;
@@ -124,7 +140,9 @@ public class WarriorMath {
 
 		return angle;
 	}
-
+	
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double boundAngleNegative180to180Degrees(double angle) {
 		while (angle >= 180.0) {
 			angle -= 360.0;
@@ -137,6 +155,8 @@ public class WarriorMath {
 		return angle;
 	}
 
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double boundAngle0to2PiRadians(double angle) {
 		while (angle >= 2.0 * Math.PI) {
 			angle -= 2.0 * Math.PI;
@@ -149,6 +169,8 @@ public class WarriorMath {
 		return angle;
 	}
 
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double boundAngleNegativePiToPiRadians(double angle) {
 		while (angle >= Math.PI) {
 			angle -= 2.0 * Math.PI;
@@ -161,22 +183,28 @@ public class WarriorMath {
 		return angle;
 	}
 
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double radiansToDegrees(double radians) {
 		return (180.0d / Math.PI) * radians;
 	}
 
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double degreesToRadians(double degrees) {
 		return (Math.PI / 180.0d) * degrees;
 	}
 
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double gate(double minimum, double value, double maximum) {
 		return Math.max(minimum, Math.min(maximum, value));
 	}
 
+	// TODO: Document
+	// TODO: Improve, clean
 	public static double map(double inputMinimum, double inputMaximum, double input, double outputMinimum, double outputMaximum) {
 		return outputMinimum + ((outputMaximum - outputMinimum) / (inputMaximum - inputMinimum)) * (input - inputMinimum);
 	}
-	
-	public WarriorMath() {
-	}
+
 }

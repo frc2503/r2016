52ca8afe1d7e71d782ecaa5bdd4b0de177e80069
Kristofer Rye <kristofer.rye@gmail.com>
2016-04-14 08:19:37 -0500

    Constants: Add a couple of constants for environment normalisation

--- src/org/usfirst/frc/team2503/r2016/Constants.java
+++ src/org/usfirst/frc/team2503/r2016/Constants.java
@@ -4,5 +4,16 @@ public class Constants {
 
 	public static final String VERSION = "1.0.0";
 	public static final String defaultHostname = "0.0.0.0";
+	
+	public static final boolean PRODUCTION = false;
+	
+	public static String USB_BASE_DIRECTORY_FILENAME;
+	
+	static {
+		if(PRODUCTION)
+			USB_BASE_DIRECTORY_FILENAME = "/U/";
+		else
+			USB_BASE_DIRECTORY_FILENAME = "./";
+	}
 
 }

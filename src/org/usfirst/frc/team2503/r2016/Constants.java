5bd0840c5bd64d1f99befe40e154249026e659b1
Kristofer Rye <kristofer.rye@gmail.com>
2016-05-20 09:18:27 -0500

    Constants: Switch to PRODUCTION mode

--- src/org/usfirst/frc/team2503/r2016/Constants.java
+++ src/org/usfirst/frc/team2503/r2016/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final String VERSION = "1.1.0";
 	public static final String defaultHostname = "0.0.0.0";
 
-	public static final boolean PRODUCTION = false;
+	public static final boolean PRODUCTION = true;
 
 	public static String USB_BASE_DIRECTORY_FILENAME;
 

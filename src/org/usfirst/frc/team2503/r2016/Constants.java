8680785ff9d919b23dc6a5916b210b2997447904
Kristofer Rye <kristofer.rye@gmail.com>
2016-05-20 09:18:46 -0500

    Bump version :arrow_up: 1.1.1

--- src/org/usfirst/frc/team2503/r2016/Constants.java
+++ src/org/usfirst/frc/team2503/r2016/Constants.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team2503.r2016;
 
 public class Constants {
 
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 	public static final String defaultHostname = "0.0.0.0";
 
 	public static final boolean PRODUCTION = true;
